54b60edf88fa695c090832f05498e2d59fcb198f
Furqat <furqaty263@gmail.com>
2025-05-18 14:11:01 +0500

    delete edit

--- src/app/[locale]/admin/ProductsComponent.jsx
+++ src/app/[locale]/admin/ProductsComponent.jsx
@@ -66,20 +66,16 @@ export default function ProductsComponent({ products, languageId, token }) {
 
         setIsLoading(true);
 
-        const res1 = await fetch(`/api/Products/DeleteProduct?productId=${productId}&languageId=${1}`, {
+        const res1 = await fetch(`/api/Products/DeleteProduct/${productId}?languageId=1`, {
             method: 'DELETE',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
+            headers: { Authorization: `Bearer ${token}` },
         });
 
         await delay(3000);
 
-        const res2 = await fetch(`/api/Products/DeleteProduct?productId=${productId}&languageId=${2}`, {
+        const res2 = await fetch(`/api/Products/DeleteProduct/${productId}?languageId=2`, {
             method: 'DELETE',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
+            headers: { Authorization: `Bearer ${token}` },
         });
 
         if (res1.ok && res2.ok) {
